abf25fc4c72b2f407cc6380553a1d836b5ec4811
md mehedi hassan <mdmehedi01326018868@gmail.com>
2026-01-25 00:00:25 +0600

    added fast time

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+ <div className="flex flex-col gap-5 min-h-screen justify-center text-center items-stretch">
+  <div className="relative">
+    <h2 className="tex-4xl font-bold">Next Js Fetching & Rendering</h2>
+    <h2 className="text-8xl absolute inset-0 -top-20 opacity-10 font-medium">Fetching Rendering & Styling</h2>
+  </div>
+  <div className="">
+    <button className="btn">Lets Explore</button>
+  </div>
+ </div>
+  );
+}
